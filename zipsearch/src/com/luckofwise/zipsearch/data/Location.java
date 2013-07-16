4ef958cedf98bcb5b1cf39937cebf0ad7adf1df5
LuckOfWise <luck.of.wise@gmail.com>
2013-07-16 13:42:50 +0900

    Locationクラス作成

---
+++ zipsearch/src/com/luckofwise/zipsearch/data/Location.java
@@ -0,0 +1,12 @@
+package com.luckofwise.zipsearch.data;
+
+public class Location {
+	String city;
+	String city_kana;
+	String town;
+	String town_kana;
+	String x;
+	String y;
+	String prefecture;
+	String postal;
+}
